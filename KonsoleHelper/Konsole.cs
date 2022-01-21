1746d2ec6a94750082d3e3d17ab486463c9f5e7c
Kyle Edward Donaldson <kyle@kedonaldson.com>
2022-01-21 16:18:03 +0000

    Add more methods and improve AskForInput

--- KonsoleHelper/Konsole.cs
+++ KonsoleHelper/Konsole.cs
@@ -4,19 +4,37 @@ namespace KonsoleHelper
 {
     public static class Konsole
     {
+        public static void WriteLines(string[] messages, int linesInBetween = 0)
+        {
+            foreach(var message in messages)
+            {
+                Console.WriteLine(message);
+
+                for (var i = 0; i < linesInBetween; i++)
+                {
+                    Console.WriteLine();
+                }
+            }
+        }
+
+        public static void WriteLines(int numberOfLines, string repeatedLine = "")
+        {
+            for (var i = 0; i < numberOfLines; i++)
+            {
+                Console.WriteLine(repeatedLine);
+            }
+        }
+
         public static void WriteLine(string message, int linesAfter = 1)
         {
             // Writes message into console
             Console.WriteLine(message);
 
             // Adds lines after message
-            for (var i = 0; i < linesAfter; i++)
-            {
-                Console.WriteLine();
-            }
+            WriteLines(linesAfter);
         }
 
-        public static T AskForInput<T>(string request, string invalidInputMessage = "Unable to parse input.")
+        public static T AskForInput<T>(string request, string invalidInputMessage = "Unable to parse input.", int linesAfter = 1)
         {
             T userInputAsT = (T)Activator.CreateInstance(typeof(T));
             bool successfulCast = false;
@@ -39,6 +57,8 @@ namespace KonsoleHelper
                     WriteLine(invalidInputMessage, 1);
                 }
             }
+
+            WriteLines(linesAfter);
 
             return userInputAsT;
         }
